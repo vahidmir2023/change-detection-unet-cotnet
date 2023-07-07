8850f4c3facc7ed9ad567734ec0617b61cfb6ccf
m <mohammad1377sharifi@gmail.com>
2023-07-08 01:51:16 +0330

    Update scripts

--- change_detection/models/models.py
+++ change_detection/models/models.py
@@ -1,28 +1,13 @@
 import torch
 import torch.nn as nn
 from .layers.initializers import he_normalizer
-from .layers.layers import CustomConv2d
-
-
-class E2EConvBlock(nn.Module):
-    def __init__(self):
-        super(E2EConvBlock, self).__init__()
-        self.seq = nn.Sequential(
-            CustomConv2d(3, 64, 5, (1,1), 2, 2),
-            # add cotlayers here in between
-            CustomConv2d(64, 128, 5, (1,1), 2, 2),
-            CustomConv2d(128, 256, 5, (1,1), 2, 2),
-            CustomConv2d(256, 512, 5, (1,1), 2, 2, dropout=0.5),
-        )
-
-    def forward(self, input):
-        return self.seq(input)
+from .layers import layers
 
 
 class SpatialAttentionBlock(nn.Module):
-    def __init__(self, conv_output):
+    def __init__(self, conv_output, reduction_factor=8):
         super(SpatialAttentionBlock, self).__init__()
-        self.c1 = conv_output.size(1) // 8
+        self.c1 = conv_output.size(1) // reduction_factor
         self.c = conv_output.size(3)
         self.d = conv_output.size(1)
         self.key_embed = self.conv1x1(d, c1)
@@ -41,32 +26,58 @@ class SpatialAttentionBlock(nn.Module):
 
     def forward(self, input):
         #TODO: finish this one
+        fa = self.key_embed(input)
+        fb = self.query_embed(input)
+        fc = self.val_embed(input)
+        fa1 = fa.view()
         pass
 
 
 class ChannelAttentionBlock(nn.Module):
-    def __init__(self, conv_output):
+    def __init__(self, reduction_factor=8):
+        self.reduction_factor = reduction_factor
+
+    def forward(self, input):
+        c1 = conv_output.size(1) // self.reduction_factor
+        c = conv_output.size(3)
+        d = conv_output.size(1)
         pass
 
-    def forward(self, input):
-        pass
 
+###### New Network ######
+class UnetCotnetNetwork(nn.Module):
+    def __init__(self, in_channels, classes):
+        super(UnetCotnetNetwork, self).__init__()
 
-class LeftBlock(nn.Module):
-    def __init__(self):
-        super(LeftBlock, self).__init__()
-        self.conv = E2EConvBlock()
+        # left side blocks
+        self.left_backbone = BackBone(in_channels)
 
-    def forward(self, input):
-        output = self.conv(input)
-        return output
-        
+        # right side blocks
+        self.right_backbone = BackBone(in_channels)
 
-class RightBlock(nn.Module):
-    def __init__(self):
-        super(RightBlock, self).__init__()
-        self.conv = E2EConvBlock()
+        # upsampling block
+        self.upsamples = nn.ModuleDict({
+            "512_512": UpsamplingBlock(512, 512, 512, 512), 
+            "256_256": UpsamplingBlock(512, 256, 256, 256), 
+            "128_128": UpsamplingBlock(256, 128, 128, 128), 
+            "128_64": UpsamplingBlock(128, 128, 64, 64, merge_upsample=False), 
+        })
 
-    def forward(self, input):
-        output = self.conv(input)
-        return output
+        self.conv_bn = nn.Sequential(
+            nn.Conv2d(64, classes, 1),
+            nn.Batchnorm2d(classes),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, left_x, right_x):
+        # Left part
+        left_bb_outputs = self.left_backbone(left_x)
+
+        # Right part
+        right_bb_outputs = self.right_backbone(right_x)
+
+        # apply spatial and channel attentions
+        # from both sides 
+
+        # Upsampling part
+        return left_bb_outputs, right_bb_outputs
