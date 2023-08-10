5437bac74a1d3b76fd3771d33c73eed732a89f6c
Mohammad Sharifi <72918092+Mohammad-sharifi@users.noreply.github.com>
2023-08-10 05:50:43 -0700

    Update dataset.py
    adding tiff cropping

--- change_detection/dataset.py
+++ change_detection/dataset.py
@@ -66,6 +66,31 @@ def make_cropped_dataset(ds_path, crop_size = (128, 128), stride = (64, 64), img
                         )
 
 
+def make_cropped_tiff_dataset(ds_path, crop_size = (128, 128), stride = (64, 64), img_format="tif"):
+    for folder in ('2010', '2017', '2D', '3D'):
+        img_folder = str(ds_path / folder)
+        cropping_folder_path = f"{ds_path}/{folder}_{crop_size[0]}_{crop_size[1]}"
+        os.makedirs(cropping_folder_path, exist_ok=True)
+        print(f"Working on {folder} TIFF images")
+        handler = imageio.v3 if folder != "3D" else tiff
+        for img_path in tqdm.tqdm(glob(img_folder + f"/*.{img_format}")):
+            img = handler.imread(img_path)
+            if img.ndim == 2:
+                img = img[:, :, np.newaxis]
+              
+            org_width, org_height, _ = img.shape
+            for x in range(0, org_width, stride[0]):
+                cropped_width = x + crop_size[0]
+                for y in range(0, org_height, stride[1]):
+                    cropped_height = y + crop_size[1]
+                    if cropped_width < org_width and cropped_height < org_height:
+                        cropped_img = img[x:cropped_width, y:cropped_height, :]
+                        basename = os.path.basename(img_path).split(".")[0]
+                        handler.imwrite(
+                            f"{cropping_folder_path}/{basename}_{x}_{cropped_width}_{y}_{cropped_height}.{img_format}",
+                            cropped_img
+                        )
+
 class PyTMinMaxScalerVectorized(object):
     """
     Transforms each channel to the range [0, 1].
