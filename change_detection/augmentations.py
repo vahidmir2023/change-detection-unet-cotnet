5b603bbf255affd668321892a01f3ffe55f4a0d4
Mohammad Sharifi <72918092+Mohammad-sharifi@users.noreply.github.com>
2023-08-09 18:10:54 -0700

    Update augmentations.py

--- change_detection/augmentations.py
+++ change_detection/augmentations.py
@@ -6,7 +6,7 @@ DS_3DCD_MAX_VALUE = 30
 DS_3DCD_MEAN = [0.5896145210542503, 0.6210658017517566, 0.591661801751776]
 DS_3DCD_STD = [0.1898555514094201, 0.19114699478664082, 0.21242997453209553]
 
-def get_training_augmentations(m = [0,0,0], s = [1,1,1]):
+def get_training_augmentations(m = DS_3DCD_MEAN, s = DS_3DCD_STD):
     transform = [
         albu.HorizontalFlip(p=0.5),
 
@@ -44,7 +44,7 @@ def get_training_augmentations(m = [0,0,0], s = [1,1,1]):
     ]
     return albu.Compose(transform, additional_targets={'t2': 'image', 'mask3d': 'mask'})
 
-def get_basic_augmentations(m = [0,0,0], s = [1,1,1]):
+def get_basic_augmentations(m = DS_3DCD_MEAN, s = DS_3DCD_STD):
     transforms = [
         albu.Normalize(mean = m, std = s),
         apt.ToTensorV2(),
